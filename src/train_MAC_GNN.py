888f9095f86c1d9c71e60925d879d50fda046785
Venus Haghighi <99381088+VenusHaghighi@users.noreply.github.com>
2023-10-20 15:11:47 +1100

    Final

--- src/train_MAC_GNN.py
+++ src/train_MAC_GNN.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 import numpy
 import argparse
 import time
-from prepare_data import Dataset
+#from prepare_data import Dataset
 from sklearn.metrics import f1_score, accuracy_score, recall_score, roc_auc_score, precision_score, confusion_matrix
 from model import *
 from sklearn.model_selection import train_test_split
@@ -36,11 +36,11 @@ def edgeLabelling_train(data, features):
     optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
        
     # Edge labelling   
-    edges = data.edges['homo']
-    edge_labels = edges.data['edge_labels']
-    edge_train_mask = edges.data['edge_train_mask'].bool()
-    edge_valid_mask = edges.data['edge_valid_mask'].bool()
-    edge_test_mask = edges.data['edge_test_mask'].bool()
+   
+    edge_labels = data.edata['edge_labels']
+    edge_train_mask = data.edata['edge_train_mask'].bool()
+    edge_valid_mask = data.edata['edge_valid_mask'].bool()
+    edge_test_mask = data.edata['edge_test_mask'].bool()
     edge_labels_float = edge_labels.type(torch.FloatTensor)
     
             
@@ -93,11 +93,12 @@ def edgeLabelling_train(data, features):
     return signed, edge_test_mask, edge_labels
 
 ### training and testing model
-def train_model(model, graph, args):
+def train_model(model, graph, graph_relation1, graph_relation2, graph_relation3, args):
     
-
         features = graph.ndata['feature']
         labels = graph.ndata['label']
+        
+        
         index = list(range(len(labels)))
         if dataset_name == 'amazon':
             index = list(range(3305, len(labels)))
@@ -120,13 +121,23 @@ def train_model(model, graph, args):
         time_start = time.time()
         theta = args.theta
         print("theta:", theta)
-        
+        linear = nn.Linear(3,1, dtype=torch.float32)
        
         
         for e in range(1000):
             
             model.train()
-            logits, final_embed = model(graph, features, theta)
+            logits_R1, final_embed_R1 = model(graph_relation1, features, theta)
+            logits_R2, final_embed_R2 = model(graph_relation2, features, theta)
+            logits_R3, final_embed_R3 = model(graph_relation3, features, theta)
+            
+            logits = torch.cat([final_embed_R1, final_embed_R2, final_embed_R3], dim=-1)
+            logits_part1 = logits[:, [0,2,4]]
+            logits_part2 = logits[:, [1,3,5]]
+            logits_part1 = linear(logits_part1)
+            logits_part2 = linear(logits_part2)
+            logits = torch.cat([logits_part1, logits_part2], dim=-1)
+            
             loss = F.cross_entropy(logits[train_mask], labels[train_mask], weight=torch.tensor([1., weight]))
             optimizer.zero_grad()
             loss.backward()
@@ -151,6 +162,7 @@ def train_model(model, graph, args):
 
         time_end = time.time()
         print('time cost: ', time_end - time_start, 's')
+        print("time cost per epoch:",(time_end - time_start)/1000, "s")
         print('Test: REC {:.2f} PRE {:.2f} MF1 {:.2f} AUC {:.2f}'.format(final_trec*100,
                                                                          final_tpre*100, final_tmf1*100, final_tauc*100))
         return final_tmf1, final_tauc, probs, preds, final_embed, test_mask
@@ -174,9 +186,9 @@ def get_best_f1(labels, probs):
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='MA2C_GNN')
-    parser.add_argument("--dataset", type=str, default="yelp",
+    parser.add_argument("--dataset", type=str, default="amazon",
                         help="Dataset for this model (yelp/amazon)")
-    parser.add_argument("--train_ratio", type=float, default=0.4, help="Training ratio")
+    parser.add_argument("--train_ratio", type=float, default=0.2, help="Training ratio")
     parser.add_argument("--hid_dim", type=int, default=64, help="Hidden layer dimension")
     parser.add_argument("--theta", type=int, default=3, help="Order theta in Beta Distribution")
     parser.add_argument("--epoch", type=int, default=100, help="The max number of epochs for graph_agnostic edge labeling module")
@@ -185,44 +197,95 @@ if __name__ == '__main__':
     args = parser.parse_args()
     print(args)
     dataset_name = args.dataset
-    dataset_path = os.getcwd() + '\\' + dataset_name + '_completed'+ '.dgl'
+    #dataset_path = os.getcwd() + '\\' + dataset_name + '_completed'+ '.dgl'
     theta = args.theta
     h_channels = args.hid_dim
     #graph = Dataset(dataset_name).graph
-    graph = dgl.load_graphs(dataset_path)[0][0]
+    graph = dgl.load_graphs(dataset_name + ".dgl")[0][0]
     in_channels = graph.ndata['feature'].shape[1]
     num_classes = 2
-    feature = graph.ndata['feature']
+    features = graph.ndata['feature']
+    print(graph)
+    print(graph.etypes)
+    relations = graph.etypes
+    ## create different sub_graphs from original graph based on the different graph relations 
+    
+#    graph_UPU = graph.edge_type_subgraph(['p'])
+#    graph_USU = graph.edge_type_subgraph(['s'])
+#    graph_UVU = graph.edge_type_subgraph(['v'])
+#    
+#    
+#   
+#    print("edgeLabelling_train starts for UPU!!!") 
+#    edgeType, edge_test_mask, edge_labels = edgeLabelling_train(graph_UPU, features) 
+#    graph_UPU.edata['edgeType'] = edgeType
+#        
+#    
+#        
+#    print("Calculating domination signals starts for UPU!!!")
+#    h2_rate, graph_UPU = connection_domination(graph_UPU)
+#    dgl.save_graphs('amazon_UPU.dgl', graph_UPU)   
+#        
+#        
+#       
+#    print("edgeLabelling_train starts for USU!!!") 
+#    edgeType, edge_test_mask, edge_labels = edgeLabelling_train(graph_USU, features) 
+#    graph_USU.edata['edgeType'] = edgeType
+#    
+#       
+#    print("Calculating domination signals starts for USU!!!")
+#    h2_rate, graph_USU = connection_domination(graph_USU)
+#    dgl.save_graphs('amazon_USU.dgl', graph_USU)     
+#        
+#        
+#    print("edgeLabelling_train starts for UVU!!!") 
+#    edgeType, edge_test_mask, edge_labels = edgeLabelling_train(graph_UVU, features) 
+#    graph_UVU.edata['edgeType'] = edgeType
+#    
+#       
+#    print("Calculating domination signals starts for UVU!!!")
+#    h2_rate, graph_UVU = connection_domination(graph_UVU)   
+#    dgl.save_graphs('amazon_UVU.dgl', graph_UVU)   
+    
+    if dataset_name == "yelp":
+    
+         graph_relation1 = dgl.load_graphs("yelp_RUR.dgl")[0][0]
+         graph_relation2 = dgl.load_graphs("yelp_RSR.dgl")[0][0]
+         graph_relation3 = dgl.load_graphs("yelp_RTR.dgl")[0][0]
     
     
+    if dataset_name == "amazon":
     
-    # #Train the edge labelling module to get the edgeType 
-    # edgeType, edge_test_mask, edge_labels = edgeLabelling_train(graph, feature) 
-    # graph.edges['homo'].data['edgeType'] = edgeType
-    
-    # # ### get sparse adjacency matrix of dgl graph
-    # sparse_adj = graph.adj(scipy_fmt='coo', etype='homo')
-    
-    # # ### get the adjacency list for all nodes in graph
-    # adj_list = sparse_to_adjlist(sparse_adj)
-    
-    # # ### get the connection domination for each node in graph
-    # h2_rate, graph = connection_domination(graph, adj_list)
+         graph_relation1 = dgl.load_graphs("amazon_UPU.dgl")[0][0]
+         graph_relation2 = dgl.load_graphs("amazon_USU.dgl")[0][0]
+         graph_relation3 = dgl.load_graphs("amazon_UVU.dgl")[0][0]
     
     
-    
-        
-        
+#    graph_UPU_r = graph_UPU
+#    graph_USU_r = graph_USU
+#    graph_UVU_r = graph_UVU
+#    
+#    size = (45954,)  # Tuple specifying the size
+#    size = (11944,)
+#    dtype = torch.int64  # Data type
+##
+#    # Create a tensor filled with random values of 1 and -1
+#    #random_h2_rate = torch.randint(0, 2, size, dtype=dtype) * 2 - 1
+#    random_h2_rate =  -1 * torch.ones(size, dtype=dtype)
+#    graph_UPU_r.ndata['h2_rate'] = random_h2_rate
+#    
+#    #random_h2_rate = torch.randint(0, 2, size, dtype=dtype) * 2 - 1
+#    graph_USU_r.ndata['h2_rate'] = random_h2_rate
+#    
+#    #random_h2_rate = torch.randint(0, 2, size, dtype=dtype) * 2 - 1
+#    graph_UVU_r.ndata['h2_rate'] = random_h2_rate
 
     if args.run == 1:
         
         model = MA2C_GNN(in_channels, h_channels, num_classes, graph, theta)
         
-        final_tmf1, final_tauc, probs, preds, final_embed, test_mask = train_model(model, graph, args)
+        final_tmf1, final_tauc, probs, preds, final_embed, test_mask = train_model(model, graph, graph_relation1, graph_relation2, graph_relation3, args)
         
-        torch.save(probs, "probs.pt")
-        torch.save(preds, "preds.pt")
-        torch.save(final_embed, "final_embed.pt")
-        torch.save(test_mask, "test_mask.pt")
+        
 
    
